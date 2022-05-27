55f9c01d65f0cda6be8bae05bdeec9122664c57e
lohith <lohith@Lohith>
2022-05-27 17:34:20 +0530

    p

---
+++ src/test/java/jenkins/test5.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test5 {
+
+	@Test
+	public void test(){
+		Reporter.log("hello Ruby",true);
+
+	}
+
+}
